8d0442d96c35b6b2049c08f8d7edccf45c0c82f5
ramin bahrami <raminbahrami@ramins-MacBook-Pro.local>
2022-10-07 19:39:26 -0400

    make it better

---
+++ public/Scripts/app.js
@@ -0,0 +1,12 @@
+// <!-- Full Name:ramin beiramzadegan  Student Id:301254240 --> 
+
+(function(){
+    function start()
+    {
+        console.log("App started...");
+    }
+    window.addEventListener("load",start);
+})();
+
+
+
